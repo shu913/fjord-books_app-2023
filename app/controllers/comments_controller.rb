34143c95cbb5648741b3f6d85d443e9b538bdbee
shu913 <sss91327@icloud.com>
2023-05-07 21:14:59 +0900

    コメントを付けられるようにする

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,37 @@
+class CommentsController < ApplicationController
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      render_commentable_show
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
